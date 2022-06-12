1a8ab93ff64cdf224bc34e49e525b19661d09e2d
samvrny <samvrny@gmail.com>
2022-06-12 15:01:25 -0500

    added lowercase array

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
