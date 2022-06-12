c64fecea0b60ebad5a452ca19433ed7c84de381f
samvrny <samvrny@gmail.com>
2022-06-12 16:16:57 -0500

    got initial window prompt working for password length

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,11 +1,12 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
 //Arrays for all characters that are selectable by the password generator
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var symbols = ["!","@","#","$","%","^","&","*","(",")","+","_","{","}","/","?","<",">","|","=","-"];
 var numbers = ["1","2","3","4","5","6","7","8","9","0"];
+
+// Add event listener to generate button
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -14,7 +15,19 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//this will be the generatePassword function
+
+//This will be the function for determining the length of the password, and which
+//characters to use. function to be named "passwordOptions"
+
+function generatePassword() {
+  passwordLength = parseInt(prompt("How many characters would you like your password to be? Please select a number between 8 - 128"));
+  if (isNaN(passwordLength) || passwordLength > 128 || passwordLength < 8) {
+    window.alert("You must pick a number between 8 - 128. Try again.");
+    generatePassword();
+  }
+};
+
+generatePassword();
