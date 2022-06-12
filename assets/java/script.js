08b1ef50d23ef7e0d8ac97e77a3aa89ecdef25ca
samvrny <samvrny@gmail.com>
2022-06-12 15:09:03 -0500

    added the rest of the arrays for characters

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,7 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Arrays for all characters that are selectable by the password generator
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var symbols = ["!","@","#","$","%","^","&","*","(",")","+","_","{","}","/","?","<",">","|","=","-"];
+var numbers = ["1","2","3","4","5","6","7","8","9","0"];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
