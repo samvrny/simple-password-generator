ae9a4131fa4486fdddf324035242c797960259c3
samvrny <samvrny@gmail.com>
2022-10-25 15:45:10 -0500

    got the CSS for the generated password to work correctly

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -16,7 +16,7 @@ function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
 
-  passwordText.innerHTML = password;
+  passwordText.textContent = password;
 };
 
 //select what characters are going to be used in the password
